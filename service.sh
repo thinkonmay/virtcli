91bf88f4294a53ece39c51db125142e7bb8728b6
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-06-24 02:29:05 +0000

    auto restart service

--- service.sh
+++ service.sh
@@ -1,12 +1,21 @@
+go build -o virtdaemon cmd/server/main.go
 systemctl stop virtdaemon.service
 echo "[Unit]
 Description=
 After=network.target
 
+StartLimitIntervalSec=500
+StartLimitBurst=5
+
+
 [Service]
 Type=simple
-ExecStart=/snap/bin/go run cmd/server/main.go
+ExecStart=/home/virtdaemon/virtdaemon
 WorkingDirectory=/home/virtdaemon
+
+Restart=always
+RestartSec=5s
+
 
 [Install]
 WantedBy=multi-user.target" > /lib/systemd/system/virtdaemon.service
